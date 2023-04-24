a89e5268c77a06042f1afaa4a1e1bbccb3ddbee4
NikTopler <nik.topler21@gmail.com>
2023-04-24 13:08:31 +0200

    Update folder structure

--- src/server/shared/lib/database/connection.ts
+++ src/server/shared/lib/database/connection.ts
@@ -7,8 +7,7 @@ const pool = mariadb.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    connectionLimit: process.env.DB_CONNECTION_LIMIT ?? 5,
+    database: process.env.DB_NAME
 });
 
 export default pool;
