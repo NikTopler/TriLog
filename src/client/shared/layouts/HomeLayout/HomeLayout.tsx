07a0f3dfd0b20483165b3f64706172d3c767c39c
NikTopler <nik.topler21@gmail.com>
2023-04-26 21:19:42 +0200

    Update HomeLayout design
    Revert "Update HomeLayout design"
    
    This reverts commit 20f26b7c61ed02f2ee8840791cd7ef7cc1e74de6.
    
    Revert "Revert "Update HomeLayout design""
    
    This reverts commit d2a7df2bdb68a0abfd3d747f024ade91df1734ea.

--- src/client/shared/layouts/HomeLayout/HomeLayout.tsx
+++ src/client/shared/layouts/HomeLayout/HomeLayout.tsx
@@ -1,20 +1,60 @@
 import { useState } from "react";
-import styles from "./HomeLayout.module.scss";
-import Navbar from "../../components/Navbar/Navbar";
-import { DisplayMode } from "../../types";
-import { LayoutProp } from "../../interfaces";
-import Sidebar from "../../components/Sidebar/Sidebar";
+import { Tooltip } from "@mui/joy";
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
-import { Tooltip } from "@mui/joy";
+import { DisplayMode } from "../../types";
+import { LayoutProp } from "../../interfaces";
+import Navbar from "../../components/Navbar/Navbar";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import styles from "./HomeLayout.module.scss";
+
+interface SidebarState {
+    open: boolean;
+    closing: boolean;
+    hovering: boolean;
+}
 
 function HomeLayout({ children }: LayoutProp) {
 
     const [mode, setMode] = useState<DisplayMode>('light');
-    const [sidebarOpen, setSidebarOpen] = useState(true);
+    const [sidebar, setSidebar] = useState<SidebarState>({
+        open: true,
+        closing: false,
+        hovering: false
+    });
+
+    const onSidebarToggle = () => {
+
+        setSidebar({
+            open: !sidebar.open,
+            closing: true,
+            hovering: false
+        });
+
+        setTimeout(() => {
+            setSidebar({
+                ...sidebar,
+                open: !sidebar.open,
+                hovering: false
+            });
+        }, 300);
+
+    }
+
+    const onSidebarHover = (hovering: boolean) => {
+
+        if (sidebar.closing) {
+            return;
+        }
+
+        setSidebar({
+            ...sidebar,
+            hovering
+        });
+    }
 
     return (
-        <div className={styles['home']} data-sidebar-open={sidebarOpen}>
+        <div className={styles['home']} data-sidebar-open={sidebar.open} data-hovering={sidebar.hovering}>
 
             <nav className={styles['home__navbar']}>
                 <div className={styles['home__navbar-main']}>
@@ -25,28 +65,33 @@ function HomeLayout({ children }: LayoutProp) {
                 </div>
             </nav>
 
-            <aside className={styles['home__sidebar']}>
-                <section className={styles['home__sidebar-main']}>
-                    <Sidebar />
-                </section>
-                <section className={styles['home__sidebar-slider']}>
-                    <div className={styles['home__sidebar-slider-content']} onClick={() => setSidebarOpen(!sidebarOpen)}>
-                        <Tooltip title={sidebarOpen ? "Close" : "Open"} placement="right">
-                            <div className={styles['home__sidebar-slider-content-icon']}>
-                                {sidebarOpen ? <KeyboardArrowLeftIcon /> : <KeyboardArrowRightIcon />}
-                            </div>
-                        </Tooltip>
+            <section className={styles['home__sidebar']}>
+                <div className={styles['home__sidebar-container']} onMouseEnter={() => onSidebarHover(true)} onMouseLeave={() => onSidebarHover(false)}>
+                    <div className={styles['home__sidebar-container__main']}>
+                        <Sidebar />
                     </div>
-                </section>
-            </aside>
-
-            <div className={styles['home__main']}>
-                <div className={styles['home__main-content']}>
-                    Home Page
+                    <div className={styles['home__sidebar-container__slider']}>
+                        <div className={styles['home__sidebar-container__slider-container']} />
+                        <div className={styles['home__sidebar-container__slider-button-container']}>
+                            <div className={styles['home__sidebar-container__slider-button-container--button']} onClick={onSidebarToggle}>
+                                <Tooltip title={sidebar.open ? 'Close' : 'Open'} placement="right">
+                                    <div className={styles['home__sidebar-slider-content-icon']}>
+                                        {sidebar.open
+                                            ? <KeyboardArrowLeftIcon />
+                                            : <KeyboardArrowRightIcon />
+                                        }
+                                    </div>
+                                </Tooltip>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            </section>
 
-            {children}
+            <section className={styles['home__content']}>
+                Home
+                {children}
+            </section>
         </div>
     );
 
