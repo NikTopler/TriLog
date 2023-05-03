fd50aef601b805005df6dca6ee18c3705d9fd091
NikTopler <nik.topler21@gmail.com>
2023-05-03 16:34:13 +0200

    Implement HomeLayout design

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,19 @@ import "@/styles/globals.scss";
 import "@/styles/auth.scss";
 
 import type { AppProps } from "next/app";
+import { HomeLayout } from "@/client/shared/layouts";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps, router }: AppProps) {
+
+  if (router.pathname.includes('/auth')) {
+    return (
+      <Component {...pageProps} />
+    );
+  }
+
+  return (
+    <HomeLayout>
+      <Component {...pageProps} />
+    </HomeLayout>
+  );
 }
