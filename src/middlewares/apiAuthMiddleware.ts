c51b6676da427f707a087855f8dc77f72e9e1f71
NikTopler <nik.topler21@gmail.com>
2023-06-30 17:44:16 +0200

    Remove unnecessary variable

--- src/middlewares/apiAuthMiddleware.ts
+++ src/middlewares/apiAuthMiddleware.ts
@@ -2,10 +2,6 @@ import getApiAuthPassword from "@/helpers/env/getApiAuthPassword";
 import getApiAuthUsername from "@/helpers/env/getApiAuthUsername";
 import { NextRequest } from "next/server";
 
-const EXCLUDED_PATHS = [
-    '/api/auth/*'
-];
-
 function apiAuthMiddleware(req: NextRequest) {
 
     const path = req.nextUrl.pathname;
