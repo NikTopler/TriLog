4cafa66e16d070de6f16dc45f69d69bc368094c3
NikTopler <nik.topler21@gmail.com>
2023-07-23 16:13:48 +0200

    Add missing Triathlon Categories

--- migrations/2023_03_12-insert_triathlon_data.sql
+++ migrations/2023_03_12-insert_triathlon_data.sql
@@ -10,21 +10,23 @@ VALUE
 ('25–29', 'm3', 'male'),
 ('30–34', 'm4', 'male'),
 ('35–39', 'm5', 'male'),
-('45–49', 'm6', 'male'),
-('50–54', 'm7', 'male'),
-('55–59', 'm8', 'male'),
-('60–64', 'm9', 'male'),
-('65–over', 'm10', 'male'),
+('40–44', 'm6', 'male'),
+('45–49', 'm7', 'male'),
+('50–54', 'm8', 'male'),
+('55–59', 'm9', 'male'),
+('60–64', 'm10', 'male'),
+('65–over', 'm11', 'male'),
 ('19–under', 'f1', 'female'),
 ('20–24', 'f2', 'female'),
 ('25–29', 'f3', 'female'),
 ('30–34', 'f4', 'female'),
 ('35–39', 'f5', 'female'),
-('45–49', 'f6', 'female'),
-('50–54', 'f7', 'female'),
-('55–59', 'f8', 'female'),
-('60–64', 'f9', 'female'),
-('65–over', 'f10', 'female');
+('40–44', 'f6', 'female'),
+('45–49', 'f7', 'female'),
+('50–54', 'f8', 'female'),
+('55–59', 'f9', 'female'),
+('60–64', 'f10', 'female'),
+('65–over', 'f11', 'female');
 
 
 INSERT INTO TriathlonTypes(name, swimKm, bikeKm, runKm, organizationID)
