eadec2a605bf97cd252643cd5328b9a9a31e9eaf
NikTopler <nik.topler21@gmail.com>
2023-08-13 16:40:22 +0200

    Update route.ts

--- src/app/api/athletes/route.ts
+++ src/app/api/athletes/route.ts
@@ -2,6 +2,16 @@ import { NextRequest, NextResponse } from "next/server";
 import { Athletes } from "@prisma/client";
 import { AthleteService, BaseService } from "@/services";
 import { AthleteFilterOptionSchema, AthleteSchema, createPaginationOptionSchema } from "@/schemas";
+import { AthleteColumns, Order, ParticipationColumns } from "@/types";
+
+const DEFAULT_PAGE = 1;
+const DEFAULT_PER_PAGE = 50;
+
+const DEFAULT_ATHLETE_ORDER: Order = 'asc';
+const DEFAULT_ATHLETE_ORDER_BY: AthleteColumns = 'lastName';
+
+const DEFAULT_PARTICIPATION_ORDER: Order = 'asc';
+const DEFAULT_PARTICIPATION_ORDER_BY: ParticipationColumns = 'rank';
 
 export async function GET(req: NextRequest) {
 
@@ -9,13 +19,42 @@ export async function GET(req: NextRequest) {
 
     try {
 
-        const validatedFilter = AthleteFilterOptionSchema.parse(searchParams);
-        const validatedPagination = createPaginationOptionSchema(BaseService.getColumnNames('Athletes') || []).parse(searchParams);
+        let countPromise, dataPromise;
+        const triathlonName = searchParams.triathlonName?.replaceAll('_', ' ');
 
-        const [count, data] = await Promise.all([
+        if (triathlonName) {
+
+            const validatedPagination = createPaginationOptionSchema<ParticipationColumns>(
+                BaseService.getColumnNames('Participations') || [],
+                DEFAULT_PER_PAGE,
+                DEFAULT_PAGE,
+                DEFAULT_PARTICIPATION_ORDER,
+                DEFAULT_PARTICIPATION_ORDER_BY
+            ).parse(searchParams);
+
+            countPromise = AthleteService.getAllByTriathlonName(triathlonName, validatedPagination, true);
+            dataPromise = AthleteService.getAllByTriathlonName(triathlonName, validatedPagination);
+
+        } else {
+
+            const validatedFilter = AthleteFilterOptionSchema.parse(searchParams);
+            const validatedPagination = createPaginationOptionSchema<AthleteColumns>(
+                BaseService.getColumnNames('Athletes') || [],
+                DEFAULT_PER_PAGE,
+                DEFAULT_PAGE,
+                DEFAULT_ATHLETE_ORDER,
+                DEFAULT_ATHLETE_ORDER_BY
+            ).parse(searchParams);
+
             AthleteService.getAll(validatedFilter, validatedPagination, true),
-            AthleteService.getAll(validatedFilter, validatedPagination)
-        ]);
+                AthleteService.getAll(validatedFilter, validatedPagination)
+
+            countPromise = AthleteService.getAll(validatedFilter, validatedPagination, true);
+            dataPromise = AthleteService.getAll(validatedFilter, validatedPagination);
+
+        }
+
+        const [count, data] = await Promise.all([countPromise, dataPromise]);
 
         return NextResponse.json({
             success: true,
