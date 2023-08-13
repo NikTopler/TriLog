763208a22250b98b46630d10f10f7ec122d3b5af
NikTopler <nik.topler21@gmail.com>
2023-08-13 16:03:06 +0200

    Update prismaClient.ts

--- src/db/prismaClient.ts
+++ src/db/prismaClient.ts
@@ -2,7 +2,7 @@ import { Prisma, PrismaClient } from "@prisma/client";
 
 const prismaClient = new PrismaClient({
     errorFormat: 'pretty',
-    log: ['query', 'info', 'warn', 'error'], 
+    log: ['warn', 'error']
 });
 
 export type TableName = Prisma.ModelName;
