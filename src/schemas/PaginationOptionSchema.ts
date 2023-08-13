0e50cf4aa492418637321157fe7480a479471add
NikTopler <nik.topler21@gmail.com>
2023-08-13 16:18:23 +0200

    Update PaginationOptionSchema.ts

--- src/schemas/PaginationOptionSchema.ts
+++ src/schemas/PaginationOptionSchema.ts
@@ -2,32 +2,37 @@ import z from "zod";
 import { PositiveIntSchema } from "./PositiveIntSchema";
 import { Order } from "@/types";
 
-export interface PaginationOptions {
+export interface PaginationOptions<T> {
     page: number;
     perPage: number;
     order: Order;
-    orderBy: string;
+    orderBy: T;
 }
 
-const DEFAULT_PER_PAGE = 25;
 const MAX_PER_PAGE = 500;
+const DEFAULT_PER_PAGE = 25;
 const DEFAULT_PAGE = 1;
 const DEFAULT_ORDER: Order = 'desc'
 const DEFAULT_SORT = 'ID';
 
-export function createPaginationOptionSchema(allowedOrderByValues: string[]) {
+export function createPaginationOptionSchema<T>(
+    allowedOrderByValues: string[],
+    perPage = DEFAULT_PER_PAGE,
+    page = DEFAULT_PAGE,
+    order = DEFAULT_ORDER,
+    orderBy = DEFAULT_SORT
+) {
 
     return z.object({
-        page: PositiveIntSchema.catch(DEFAULT_PAGE),
-        perPage: PositiveIntSchema.max(MAX_PER_PAGE).catch(DEFAULT_PER_PAGE),
-        order: z.enum(['asc', 'desc']).transform((value) => value.toLowerCase() as Order).catch(DEFAULT_ORDER),
-        orderBy: z.string().default(DEFAULT_ORDER).transform((value) => {
-
+        page: PositiveIntSchema.catch(page),
+        perPage: PositiveIntSchema.max(MAX_PER_PAGE).catch(perPage),
+        order: z.enum(['asc', 'desc']).transform((value) => value.toLowerCase() as Order).catch(order),
+        orderBy: z.string().default(orderBy).transform((value) => {
             if (!value || !allowedOrderByValues.includes(value)) {
-                return DEFAULT_SORT;
+                return orderBy as T;
             }
 
-            return value;
+            return value as T;
         })
     });
 
