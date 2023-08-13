59810d8e7ae58f091e31a09ca42c905a72d9c5de
NikTopler <nik.topler21@gmail.com>
2023-08-14 00:11:00 +0200

    Update index.ts

--- src/components/tables/columns/index.ts
+++ src/components/tables/columns/index.ts
@@ -1,9 +1,13 @@
+import participationColumns, { ParticipationTable, SpecificTriathlonParticipationColumns } from "./participationColumns";
 import triathlonColumns, { TriathlonTable } from "./triathlonColumns";
 
 export type {
-    TriathlonTable
+    TriathlonTable,
+    ParticipationTable,
+    SpecificTriathlonParticipationColumns
 }
 
 export {
-    triathlonColumns
+    triathlonColumns,
+    participationColumns
 }
